7eda58b6a2956c31cd8581294537a1cc07c9db4d
Victor <victordosanjos19@gmail.com>
2023-09-06 23:06:27 -0300

    adding my Pokemon class at function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,17 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-          <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}" >
+          <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
 
              <div class="detail">
                <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
               </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg", alt="${pokemon.name}">
+                <img src="${pokemon.photo}", alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -25,8 +22,8 @@ const pokemonList = document.getElementById('pokemonList')
 
 
 
-    PokeAPI.getPokemons().then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML = newHtml
-    })
-  
+PokeAPI.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+})
+
